23d1214a6a4a0cb46243ab0b151fcb1a371bf8d7
Jascha <jascha@jaeh.at>
2020-01-10 23:43:43 +0100

    format

--- test/spec.mjs
+++ test/spec.mjs
@@ -4,7 +4,11 @@ import webboot from '../src/index.mjs'
 
 export default [
   { fn: () => webboot, expect: is.fn, info: 'webboot is a function' },
-  { fn: () => webboot, expect: is.deep.eq(webboot.verify), info: 'webboot is equivalent to webboot.verify' },
+  {
+    fn: () => webboot,
+    expect: is.deep.eq(webboot.verify),
+    info: 'webboot is equivalent to webboot.verify',
+  },
   { fn: () => webboot.generate, expect: is.fn, info: 'webboot.generate is a function' },
   { fn: () => webboot.verify, expect: is.fn, info: 'webboot.verify is a function' },
   { fn: () => webboot.sign, expect: is.fn, info: 'webboot.sign is a function' },
