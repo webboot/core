b19851a1b8c6144df4b139077ae1db9ceb57c08a
Jascha <jascha@jaeh.at>
2020-01-11 00:52:40 +0100

    sri-hashes.json is an object

--- src/tasks/write.mjs
+++ src/tasks/write.mjs
@@ -7,7 +7,7 @@ export const write = async state => {
     .map(({ algorithm, url, hash }) => `  "${url}": "${algorithm}-${hash}"`)
     .join(',\n')
 
-  const hashString = `[\n${hashes}\n]`
+  const hashString = `{\n${hashes}\n}`
 
   const sriHashFile = path.join(state.dir, 'sri-hashes.json')
   const written = await fs.writeFile(sriHashFile, hashString)
