fe7e70de0c8fc8b55d4c2b371c1cc9bd7e2f7d04
Jascha <jascha@jaeh.at>
2020-01-11 02:27:48 +0100

    clean only errors if error is not ENOENT

--- src/tasks/clean.mjs
+++ src/tasks/clean.mjs
@@ -1,3 +1,14 @@
 import fs from '@magic/fs'
 
-export const clean = async state => await fs.rmrf(state.sri)
+export const clean = async state => {
+  try {
+    await fs.rmrf(state.sri)
+    return true
+  } catch(e) {
+    if (e.code === 'ENOENT') {
+      return true
+    }
+
+    return e
+  }
+}
