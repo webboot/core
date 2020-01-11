536356fcec6dc85c8673c49409ca78674abcfa76
Jascha <jascha@jaeh.at>
2020-01-11 01:05:08 +0100

    tasks/verify: use lib.fileHash, use state.sri

--- src/tasks/generate.mjs
+++ src/tasks/generate.mjs
@@ -1,13 +1,13 @@
 import log from '@magic/log'
 
-import { getFiles, createFileHash } from './lib/index.mjs'
+import { getFiles, fileHash } from './lib/index.mjs'
 
 export const generate = async state => {
   const startTime = log.hrtime()
 
   const files = await getFiles(state)
 
-  state.files = files.map(file => createFileHash(file))
+  state.files = files.map(file => fileHash.create(file))
 
   log.timeTaken(startTime, '@webboot/core generate took:')
 
--- src/tasks/verify.mjs
+++ src/tasks/verify.mjs
@@ -1,24 +1,17 @@
 import log from '@magic/log'
 import fs from '@magic/fs'
 
-import { getFiles, createFileHash } from '../lib/index.mjs'
+import { getFiles, fileHash } from './lib/index.mjs'
 
 export const verify = async state => {
   const startTime = log.hrtime()
 
   state.files = await getFiles(state)
 
-  const sriHashFile = path.join(state.dir, 'sri-hashes.json')
-  const sriHashString = await fs.readFile(sriHashFile, 'utf8');
+  const sriHashString = await fs.readFile(state.sri, 'utf8')
   const sriHashes = JSON.parse(sriHashString)
 
-  const valid = state.files.map(file => {
-    const hash = createFileHash(file)
-
-    //const fileHash = sriHashes.first(a => )
-
-    return hash === file.hash
-  })
+  state.files.map(fileHash.check(sriHashes))
 
   log.timeTaken(startTime, '@webboot/core verify took:')
 
