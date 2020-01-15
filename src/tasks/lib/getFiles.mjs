17a15f71c819fd4e7faa178dd38509a833bf2281
Jascha <jascha@jaeh.at>
2020-01-15 07:57:31 +0100

    getFiles: make more robust, throw errors

--- src/tasks/lib/getFiles.mjs
+++ src/tasks/lib/getFiles.mjs
@@ -1,20 +1,38 @@
 import fs from '@magic/fs'
 import is from '@magic/types'
+import error from '@magic/error'
 
-export const getFiles = async state => {
+export const getFiles = async (state = {}) => {
+  if (is.empty(state)) {
+    throw error('state can not be empty', 'E_STATE_EMPTY')
+  }
+
   if (!is.empty(state.files)) {
     return state.files
   }
 
-  const filePaths = await fs.getFiles(state.dir)
+  const stat = await fs.stat(state.dir)
+
+  if (!stat.isDirectory()) {
+    throw error('getFiles expects a directory, not a file', 'E_NOT_A_DIR')
+  }
+
+  const filePaths = await fs.getFiles(state.dir, true)
 
   const filePromises = filePaths
     .filter(file => !file.endsWith(state.sri))
-    .map(async file => ({
-      file,
-      url: file.replace(state.dir, '').replace('index.html', '') || '/',
-      content: await fs.readFile(file, 'utf8'),
-    }))
+    .map(async file => {
+      let url = file
+        .replace(state.dir, '')
+        .replace('index.html', '')
+        .replace('.html', '/')
+
+      return {
+        file,
+        url,
+        content: await fs.readFile(file, 'utf8'),
+      }
+    })
 
   return await Promise.all(filePromises)
 }
