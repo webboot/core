9f9e7c098ce90eedb4e094fad6bbe7d38366ee7c
Jascha <jascha@jaeh.at>
2020-01-16 01:12:22 +0100

    tasks/clean: better error handling

--- src/tasks/clean.mjs
+++ src/tasks/clean.mjs
@@ -1,14 +1,21 @@
 import fs from '@magic/fs'
+import error from '@magic/error'
+import is from '@magic/types'
+
+const libName = '@weboot/core.tasks.clean'
 
 export const clean = async state => {
-  try {
-    await fs.rmrf(state.sri)
-    return true
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      return true
-    }
-
-    return e
+  if (is.empty(state)) {
+    throw error(`${libName} expects argument to be an object`, 'E_ARG_EMPTY')
   }
+
+  if (is.empty(state.sri)) {
+    throw error(`${libName} expects state.sri to be non-empty`, 'E_ARG_EMPTY')
+  }
+
+  if (!is.string(state.sri)) {
+    throw error(`${libName} expects state.sri to be a string`, 'E_ARG_TYPE')
+  }
+
+  return await fs.rmrf(state.sri)
 }
