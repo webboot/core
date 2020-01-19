f3e7be191927f22792de4cd533abbf7a044c6a11
Jascha <jascha@jaeh.at>
2020-01-19 03:41:00 +0100

    tasks/release: better error, add comments

--- src/tasks/release.mjs
+++ src/tasks/release.mjs
@@ -1,7 +1,14 @@
 import log from '@magic/log'
 
+const libName = `@webboot/core.tasks.release`
+
 export const release = async state => {
-  log.error('ENOTIMPLEMENTED', '@webboot/core release not implemented yet.')
+  log.error('E_NOT_IMPLEMENTED', `${libName} not implemented yet.`)
+
+  // url    - url of the homepage
+  // hashes - list of files and hashes for each file
+  // git    - git repository url git@git(hub|lab).com/username/repository
+  // user   - later, optional, online identifier of the public key.
 
   return state
 }
