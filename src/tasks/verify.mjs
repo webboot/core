f36b196668374d1968c772bd1792c027464977a0
Jascha <jascha@jaeh.at>
2020-01-19 03:42:04 +0100

    tasks/verify: handle input more robustly.

--- src/tasks/verify.mjs
+++ src/tasks/verify.mjs
@@ -1,33 +1,64 @@
-import log from '@magic/log'
+import path from 'path'
+
+import error from '@magic/error'
 import fs from '@magic/fs'
+import log from '@magic/log'
+import is from '@magic/types'
 
 import crypto from '@webboot/crypto'
 
-import { getFiles } from './lib/index.mjs'
+import { getFiles, json } from '../lib/index.mjs'
+
+const libName = '@webboot/core.tasks.verify'
+
+export const verifyFile = sriHashes => file => {
+  const fileHash = crypto.hash.create(file.content)
+
+  let argumentValid = true
+
+  if (file.hash) {
+    const { algorithm } = file
+    argumentValid = crypto.hash.check(file.content, file.hash, { algorithm })
+  }
+
+  const valid = crypto.hash.check(file.content, fileHash.hash)
+
+  const sri = sriHashes[file.url]
+  const algoHash = `${file.algorithm}-${file.hash}`
+
+  const mismatch = sri !== algoHash
+
+  return !valid || !argumentValid || mismatch
+}
 
 export const verify = async state => {
   const startTime = log.hrtime()
 
+  if (is.empty(state)) {
+    throw error(`${libName} state can not be empty.`, 'E_STATE_EMPTY')
+  }
+
+  if (!is.string(state.sri)) {
+    throw error(`${libName} expects state.sri to be set.`, 'E_STATE_KEY_MISSING')
+  }
+
+  if (!path.isAbsolute(state.sri)) {
+    state.sri = path.join(state.cwd, state.sri)
+  }
+
   state.files = await getFiles(state)
 
   const sriHashString = await fs.readFile(state.sri, 'utf8')
-  const sriHashes = JSON.parse(sriHashString)
+  const sriHashes = json.parse(sriHashString)
 
   const mismatches = state.files
-    .filter(file => {
-      const invalid = !crypto.hash.check(file.content, file.hash)
-
-      const sri = sriHashes[file.url]
-      const algoHash = `${file.algorithm}-${file.hash}`
-
-      const mismatch = sri !== algoHash
-
-      return invalid || mismatch
-    })
+    .filter(file => !file.url.startsWith('sri-') && !file.url.endsWith('.json'))
+    .filter(verifyFile(sriHashes))
     .map(f => f.file)
 
   if (mismatches.length) {
-    return error(`file hash mismatches: \n${mismatches.join('\n')}`, 'E_HASH_MISMATCH')
+    const mismatchString = mismatches.join('\n')
+    throw error('file hash mismatches:\n' + mismatchString, 'E_HASH_MISMATCH')
   }
 
   log.timeTaken(startTime, '@webboot/core verify took:')
