7aa3424b328cb809ac5e91786a3403f4dad60b71
Jascha <jascha@jaeh.at>
2020-01-19 07:17:43 +0100

    moar errorMessages

--- src/errorMessages.mjs
+++ src/errorMessages.mjs
@@ -5,14 +5,24 @@ export const errorMessages = libName => ({
   E_STATE_SRI_EMPTY: [`${libName} state.sri must be non-empty.`, 'STATE_SRI_EMPTY'],
   E_STATE_SRI_TYPE: [`${libName} state.sri must be a string.`, 'STATE_SRI_TYPE'],
 
-  E_STATE_DIR_EMPTY: [`${libName} state.dir must be non-empty.`, 'E_STATE_DIR_EMPTY'],
-  E_STATE_DIR_TYPE: [`${libName} state.dir must be a string.`, 'E_STATE_DIR_TYPE'],
+  E_STATE_DIR_EMPTY: [`${libName} state.dir must be non-empty.`, 'STATE_DIR_EMPTY'],
+  E_STATE_DIR_TYPE: [`${libName} state.dir must be a string.`, 'STATE_DIR_TYPE'],
 
-  E_NOT_A_DIR: [`${libName} expects a directory.`, 'E_NOT_A_DIR'],
+  E_NOT_A_DIR: [`${libName} expects a directory.`, 'NOT_A_DIR'],
 
-  E_STATE_PUB_KEY_EMPTY: ['${libName} state.pubKey must be non-empty.', 'E_STATE_PUB_KEY_EMPTY'],
-  E_STATE_PUB_KEY_TYPE: ['${libName} state.pubKey must be a string.', 'E_STATE_PUB_KEY_TYPE'],
-  E_STATE_PUB_KEY_FILE_EXISTS: ['${libName} state.pubKey does not point to an existing file.', 'E_STATE_PUB_KEY_FILE_EXISTS'],
+  E_STATE_PUB_KEY_EMPTY: ['${libName} state.pubKey must be non-empty.', 'STATE_PUB_KEY_EMPTY'],
+  E_STATE_PUB_KEY_TYPE: ['${libName} state.pubKey must be a string.', 'STATE_PUB_KEY_TYPE'],
+  E_STATE_PUB_KEY_FILE_EXISTS: [
+    '${libName} state.pubKey does not point to an existing file.',
+    'STATE_PUB_KEY_FILE_EXISTS',
+  ],
 
-  E_STATE_PASSPHRASE_EMPTY: ['${libName} state.passphrase is required.', 'E_STATE_PASSPHRASE_EMPTY']
+  E_STATE_PASSPHRASE_EMPTY: [
+    '${libName} state.passphrase must be non-empty.',
+    'STATE_PASSPHRASE_EMPTY',
+  ],
+  E_STATE_PASSPHRASE_TYPE: [
+    '${libName} state.passphrase must be a string.',
+    'E_STATE_PASSPHRASE_TYPE',
+  ],
 })
