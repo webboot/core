30326019af89610099fc5422bf3f2dcef8d3aaa1
Jascha <jascha@jaeh.at>
2020-01-20 23:22:23 +0100

    update errorMessages

--- src/errorMessages.mjs
+++ src/errorMessages.mjs
@@ -12,9 +12,10 @@ export const errorMessages = libName => ({
 
   E_STATE_PUB_KEY_EMPTY: ['${libName} state.pubKey must be non-empty.', 'STATE_PUB_KEY_EMPTY'],
   E_STATE_PUB_KEY_TYPE: ['${libName} state.pubKey must be a string.', 'STATE_PUB_KEY_TYPE'],
-  E_STATE_PUB_KEY_FILE_EXISTS: [
+
+  E_PUB_KEY_FILE_ENOENT: [
     '${libName} state.pubKey does not point to an existing file.',
-    'STATE_PUB_KEY_FILE_EXISTS',
+    'PUB_KEY_FILE_ENOENT',
   ],
 
   E_STATE_PASSPHRASE_EMPTY: [
@@ -23,6 +24,9 @@ export const errorMessages = libName => ({
   ],
   E_STATE_PASSPHRASE_TYPE: [
     '${libName} state.passphrase must be a string.',
-    'E_STATE_PASSPHRASE_TYPE',
+    'STATE_PASSPHRASE_TYPE',
   ],
+
+  E_PUB_IS_PRIV_KEY: ['state.publicKey is a private key file. do not do that again please.', 'E_PUB_IS_PRIV_KEY'],
+  E_PUB_NOT_A_KEY: ['state.publicKey is not a public key file.', 'E_PUB_NOT_A_KEY'],
 })
