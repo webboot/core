96e03e792fbdb135e3c2099d09e55c6c41b8ebc7
Jascha <jascha@jaeh.at>
2020-01-21 02:06:01 +0100

    errorMessages: use Object.fromEntries to avoid value duplication

--- src/errorMessages.mjs
+++ src/errorMessages.mjs
@@ -1,32 +1,36 @@
-export const errorMessages = libName => ({
-  E_STATE_EMPTY: [`${libName} state must be non-empty.`, 'STATE_EMPTY'],
-  E_STATE_TYPE: [`${libName} state must be an object.`, 'STATE_TYPE'],
+import error from '@magic/error'
+import is from '@magic/types'
 
-  E_STATE_SRI_EMPTY: [`${libName} state.sri must be non-empty.`, 'STATE_SRI_EMPTY'],
-  E_STATE_SRI_TYPE: [`${libName} state.sri must be a string.`, 'STATE_SRI_TYPE'],
+export const mapMessageToError = ([name, msg]) => [name, is.fn(msg) ? msg : [msg, name]]
 
-  E_STATE_DIR_EMPTY: [`${libName} state.dir must be non-empty.`, 'STATE_DIR_EMPTY'],
-  E_STATE_DIR_TYPE: [`${libName} state.dir must be a string.`, 'STATE_DIR_TYPE'],
+export const errorMessages = libName =>
+  Object.fromEntries(
+    [
+      ['STATE_EMPTY', `${libName} state must be non-empty.`],
+      ['STATE_TYPE', `${libName} state must be an object.`],
 
-  E_NOT_A_DIR: [`${libName} expects a directory.`, 'NOT_A_DIR'],
+      ['STATE_SRI_EMPTY', `${libName} state.sri must be non-empty.`],
+      ['STATE_SRI_TYPE', `${libName} state.sri must be a string.`],
 
-  E_STATE_PUB_KEY_EMPTY: ['${libName} state.pubKey must be non-empty.', 'STATE_PUB_KEY_EMPTY'],
-  E_STATE_PUB_KEY_TYPE: ['${libName} state.pubKey must be a string.', 'STATE_PUB_KEY_TYPE'],
+      ['STATE_DIR_EMPTY', `${libName} state.dir must be non-empty.`],
+      ['STATE_DIR_TYPE', `${libName} state.dir must be a string.`],
 
-  E_PUB_KEY_FILE_ENOENT: [
-    '${libName} state.pubKey does not point to an existing file.',
-    'PUB_KEY_FILE_ENOENT',
-  ],
+      ['NOT_A_DIR', `${libName} expects a directory.`],
 
-  E_STATE_PASSPHRASE_EMPTY: [
-    '${libName} state.passphrase must be non-empty.',
-    'STATE_PASSPHRASE_EMPTY',
-  ],
-  E_STATE_PASSPHRASE_TYPE: [
-    '${libName} state.passphrase must be a string.',
-    'STATE_PASSPHRASE_TYPE',
-  ],
+      ['STATE_PUB_KEY_EMPTY', `${libName} state.pubKey must be non-empty.`],
+      ['STATE_PUB_KEY_TYPE', `${libName} state.pubKey must be a string.`],
 
-  E_PUB_IS_PRIV_KEY: ['state.publicKey is a private key file. do not do that again please.', 'E_PUB_IS_PRIV_KEY'],
-  E_PUB_NOT_A_KEY: ['state.publicKey is not a public key file.', 'E_PUB_NOT_A_KEY'],
-})
+      ['PUB_KEY_FILE_ENOENT', `${libName} state.pubKey does not point to an existing file.`],
+
+      ['STATE_PASSPHRASE_EMPTY', `${libName} state.passphrase must be non-empty.`],
+      ['STATE_PASSPHRASE_TYPE', `${libName} state.passphrase must be a string.`],
+
+      ['PUB_IS_PRIV_KEY', `${libName} state.publicKey is a private key file. do not do that.`],
+      ['PUB_NOT_A_KEY', `${libName} state.publicKey is not a public key file.`],
+
+      ['HTTP_URL_EMPTY', `${libName} url (1st fn arg) can not be empty.`],
+      ['HTTP_URL_TYPE', `${libName} url (1st fn arg) can not be empty.`],
+
+      ['HASH_MISMATCH', arg => [`file hash mismatches:\n${arg}`, 'HASH_MISMATCH']],
+    ].map(mapMessageToError),
+  )
