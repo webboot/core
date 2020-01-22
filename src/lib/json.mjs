a2286dbd958cda9b32b95f735efeab2f89dc0edf
Jascha <jascha@jaeh.at>
2020-01-22 22:26:23 +0100

    lib/json: use errorMessages

--- src/lib/json.mjs
+++ src/lib/json.mjs
@@ -1,14 +1,18 @@
 import error from '@magic/error'
 import is from '@magic/types'
 
+import { errorMessages } from '../errorMessages.mjs'
+
 const libName = '@webboot/core.lib.json'
+
+export const errors = errorMessages(libName)
 
 export const parse = (thing, ...rest) => {
   try {
     return JSON.parse(thing, ...rest)
   } catch (e) {
     if (e.name === 'SyntaxError') {
-      e = [`${libName}.parse:\n${e.message}`, 'JSON_PARSE']
+      e = errors.JSON_PARSE(e.message)
     }
 
     throw error(e)
