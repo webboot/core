d86e9fad2886879ea6715f92dd5bb6073bc98d10
Jascha <jascha@jaeh.at>
2020-01-22 23:31:03 +0100

    tasks/sign: use getEmail and getPgpKey

--- src/tasks/sign.mjs
+++ src/tasks/sign.mjs
@@ -1,11 +1,10 @@
-import path from 'path'
-
 import log from '@magic/log'
-import fs from '@magic/fs'
 import error from '@magic/error'
 import is from '@magic/types'
 
 import crypto from '@webboot/crypto'
+
+import { exec, getEmail, getPgpKey } from '../lib/index.mjs'
 
 import { errorMessages } from '../errorMessages.mjs'
 
@@ -19,68 +18,41 @@ export const sign = async state => {
   const startTime = log.hrtime()
 
   if (is.empty(state)) {
-    throw error(...errors.E_STATE_EMPTY)
+    throw error(errors.STATE_EMPTY)
   }
 
   if (!is.objectNative(state)) {
-    throw error(errors.E_STATE_TYPE)
+    throw error(errors.STATE_TYPE)
   }
 
-  const { pass } = state
-  const pubKeyFile = {
-    path: state.publicKey,
+  if (is.empty(state.username)) {
+    throw error(errors.STATE_USERNAME_EMPTY)
   }
 
-  if (is.empty(pubKeyFile.path)) {
-    throw error(errors.E_STATE_PUB_KEY_EMPTY)
+  if (!is.string(state.username)) {
+    throw error(errors.STATE_USERNAME_TYPE)
   }
 
-  if (!is.string(pubKeyFile.path)) {
-    throw error(errors.E_STATE_PUB_KEY_TYPE)
+  state.email = await getEmail(state)
+
+  state.key = await getPgpKey(state)
+
+  // nice to have:
+  // get user passphrase interactively, launch a worker process for that.
+  // only send the hash back to this process.
+  if (is.empty(state.passphrase)) {
+    throw error(error.STATE_PASSPHRASE_EMPTY)
   }
 
-  if (!path.isAbsolute(pubKeyFile.path)) {
-    pubKeyFile.path = path.join(cwd, pubKeyFile.path)
+  if (!is.string(state.passphrase)) {
+    throw error(error.STATE_PASSPHRASE_EMPTY)
   }
 
-  if (is.empty(pass)) {
-    throw error(errors.E_STATE_PASSPHRASE_EMPTY)
-  }
+  // const hashed = crypto.hash.create(content)
 
-  if (!is.string(pass)) {
-    throw error(errors.E_STATE_PASSPHRASE_TYPE)
-  }
+  // const secret = `${passphrase} ${pubKeyContent.trim()} ${hashed.algorithm}-${hashed.hash}`
 
-  if (!pubKeyFile.path.endsWith('.pub')) {
-    const pubExists = await fs.exists(`${pubKeyFile.path}.pub`)
-    if (pubExists) {
-      log.warn('W_PRIV_KEY', [
-        '${libName}: ${pubKeyFile.path} seems to point to a private key file.',
-        'To make sure @webboot never reads a private key file by accident,',
-        'we will instead read ${pubKeyFile.path}.pub',
-        'If this results in an error, please file an issue with your use case.',
-        'https://github.com/webboot/core/',
-      ])
-
-      pubKeyFile.path = `${pubKeyFile.path}.pub`
-    }
-  }
-
-  const content = await fs.readFile(pubKeyFile.path, 'utf8')
-
-  if (content.includes('PRIVATE KEY')) {
-    // clean content variable to remove private key. at least try to, it's node.
-    content = ''
-
-    return error('--key was passed a private key file path. please do not do that.', 'E_PRIV_KEY')
-  }
-
-  const hashed = crypto.hash.create(content)
-
-  const secret = `${pass} ${content.trim()} ${hashed.algorithm}-${hashed.hash}`
-
-  const keys = crypto.ecdh(secret)
-  console.log({ keys })
+  // const keys = crypto.ecdh(secret)
 
   log.timeTaken(startTime, '@webboot/core sign took:')
 
