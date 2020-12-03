b8f1b65f42ae193cc2c4c1a2f5c6748b5fce3112
jascha <jascha@jaeh.at>
2020-12-03 06:38:01 +0100

    lib/numericPrompt: expect items to get passed and return selected item. throw error if needed

--- src/lib/numericPrompt.mjs
+++ src/lib/numericPrompt.mjs
@@ -1,10 +1,31 @@
+import cli from '@magic/cli'
 import is from '@magic/types'
 
-import cli from '@magic/cli'
+import error from '@magic/error'
+import { errorMessages } from '../errorMessages.mjs'
+const libName = '@webboot/core.lib.numericPrompt'
+export const errors = errorMessages(libName)
 
-export const numericPrompt = async (max = 10) => {
-  const msg = `Please enter a number between 1 and ${max}:`
-  const keyId = await cli.prompt(msg)
+export const numericPrompt = async (args = {}) => {
+  const { items = [], msg = '', firstRun = true } = args
+  const max = items.length
+
+  if (!max) {
+    throw error(errors.NUMERIC_PROMPT_NO_ITEMS)
+  }
+
+  if (firstRun) {
+    if (msg) {
+      log(msg)
+    }
+
+    items.forEach((item, i) => {
+      log.warn(i + 1, ' - ', item)
+    })
+  }
+
+  const question = `Please enter a number between 1 and ${max}:`
+  const keyId = await cli.prompt(question)
 
   keyId = parseInt(keyId)
 
@@ -14,15 +35,15 @@ export const numericPrompt = async (max = 10) => {
 
   if (notANum || tooSmall || tooBig) {
     if (notAnum) {
-      log.warn('Not a number', msg)
+      log.warn('Not a number', keyId, msg)
     } else if (tooSmall) {
-      log.warn('Number too small', msg)
+      log.warn('Number too small', keyId, msg)
     } else if (tooBig) {
-      log.warn('Number too big', msg)
+      log.warn('Number too big', keyId, msg)
     }
 
-    return await numericPrompt(k)
+    return await numericPrompt({ items, msg, firstRun: false })
   }
 
-  return keyId - 1
+  return items[keyId - 1]
 }
